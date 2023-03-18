05e24013dd0f7f64809766c9f3e8024916ef4ef9
MinaSamirSaad <minasamir.nashid@gmail.com>
2023-03-18 03:34:56 +0200

    program that prints all single digit numbers of base 10 starting from 0

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase, except for q and e.
+ *
+ * Return: Always 0 (success).
+ */
+int main(void)
+{
+int number;
+
+for (number = 0;  number  < 10; number++)
+printf("%d", number);
+
+return (0);
+}
