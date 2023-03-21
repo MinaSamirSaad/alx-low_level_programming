15fb8d3332e3beafb3ee9b0655e5b1230b71c4a5
MinaSamirSaad <minasamir.nashid@gmail.com>
2023-03-21 22:07:07 +0200

    program that prints _putchar, followed by a new line

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,24 @@
-#include<stdio.h>
+#include "main.h"
+
 /**
- * main - Prints a random number and states whether
- *        it is positive, negative, or zero.
+ * main - prints _putchar followed by new line
  *
- * Return: Always 0.
-*/
-int main(void) 
+ * Description: prints _putchar
+ *
+ * Return: 0
+ */
+
+int main(void)
 {
-printf("_putchar\n");
-return (0);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
 }
