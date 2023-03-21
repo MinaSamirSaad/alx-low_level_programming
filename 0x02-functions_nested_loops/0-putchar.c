03ba195432f7af68ad4f63111d422581af812408
MinaSamirSaad <minasamir.nashid@gmail.com>
2023-03-21 21:04:10 +0200

    program that prints _putchar, followed by a new line

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+ * main - Prints a random number and states whether
+ *        it is positive, negative, or zero.
+ *
+ * Return: Always 0.
+*/
+int main(void) 
+{
+printf("_putchar\n");
+return (0);
+}
