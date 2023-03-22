86340755f1472a734695da2b2223cac26b9f2074
MinaSamirSaad <minasamir.nashid@gmail.com>
2023-03-22 19:21:40 +0200

    prints the sum of all the multiples of 3 or 5

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+/**
+ * main - prints the sum
+ *
+ * Description: prints the sum of all the multiples of 3 or 5
+ *
+ * Return: 0
+ */
+int main(void)
+{
+int result, n;
+for (n = 0; n < 1024 ; n++)
+{
+if (n % 3 == 0 || n % 5 == 0)
+result += n;
+}
+return (0);
+}
