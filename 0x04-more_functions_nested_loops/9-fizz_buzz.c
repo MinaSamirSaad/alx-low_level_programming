362a4b8d0feda3888371a5e0918c87e5027fbf34
MinaSamirSaad <minasamir.nashid@gmail.com>
2023-03-23 18:16:03 +0200

    Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,9 +10,9 @@ int main(void)
 int i;
 for (i = 1; i <= 100; i++)
 {
-if (i % 3 == 0)
+if (i % 3 == 0 && i % 5 != 0)
 printf(" Fizz");
-else if (i % 5 == 0)
+else if (i % 5 == 0 && i % 3 != 0)
 printf(" Buzz");
 else if (i % 15 == 0)
 printf(" FizzBuzz");
