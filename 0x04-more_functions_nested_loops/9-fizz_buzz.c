ec2300bbd97f62ae9a8cd1c808c9f81b2ed5ab49
MinaSamirSaad <minasamir.nashid@gmail.com>
2023-03-23 18:12:09 +0200

    Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,13 +11,15 @@ int i;
 for (i = 1; i <= 100; i++)
 {
 if (i % 3 == 0)
-printf("Fizz ");
+printf(" Fizz");
 else if (i % 5 == 0)
-printf("Buzz ");
+printf(" Buzz");
 else if (i % 15 == 0)
-printf("FizzBuzz ");
+printf(" FizzBuzz");
+else if (i == 1)
+printf("%d", i);
 else
-printf("%d ", i);
+printf(" %d", i);
 }
 printf("\n ");
 return (0);
