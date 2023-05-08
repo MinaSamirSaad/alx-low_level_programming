fe7d5c4cc47af54a27b0b9aaca3a461256b89360
MinaSamirSaad <minasamir.nashid@gmail.com>
2023-05-09 01:00:42 +0300

    function that reads a text file and prints it to the POSIX standard output

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,31 +1,27 @@
 #include "main.h"
 /**
-* read_textfile - Function that reads a text file and prints it
-*@filename: file name
+ * read_textfile - Function that reads a text file and prints it
+ *@filename: file name
 *@letters: is the number of letters it should read and print
 *Return: he actual number of letters it could read and print
 */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 FILE *fptr;
-char ch;
-ssize_t counter;
-if(filename == NULL)
+char *ch;
+ssize_t read_count, print_count;
+if (filename == NULL)
 return (0);
-fptr = fopen(filename, "r");
-if (fptr == NULL)
+fptr = open(filename, O_RDONLY);
+if (fptr == -1)
 return (0);
-while (ch != EOF)
-{
-if (counter != letters)
-{
-ch = fgetc(fptr);
-_putchar(ch);
-}
-counter++;
-}
+ch = malloc(letters);
+if (ch == NULL)
+return (0);
+read_count = read(fptr, ch, letters);
+print_count = write(STDOUT_FILENO, ch, read_count);
 fclose(fptr);
-if(counter < letters)
+if (print_count != read_count)
 return (0);
-return (counter);
+return (print_count);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,9 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
+
+#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stddef.h>
-#include <sys/types.h>
+#include <fcntl.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
