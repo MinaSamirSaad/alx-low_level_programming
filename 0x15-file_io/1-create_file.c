9a7f8bcd2fc638527232916266235535a4d9b834
MinaSamirSaad <minasamir.nashid@gmail.com>
2023-05-09 01:53:23 +0300

    function that creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -7,7 +7,7 @@
 */
 int create_file(const char *filename, char *text_content)
 {
-int file = -1, length = 0, close_flag,  written = 0;
+int file = -1, length = 0,  written = 0;
 if (filename == NULL)
 return (-1);
 file = open(filename, O_RDWR | O_CREAT | O_WRONLY, S_IRUSR | S_IWUSR);
@@ -22,8 +22,7 @@ length++;
 }
 written = write(file, text_content, length);
 }
-close_flag = close(file);
-if (written == -1 || written != length || close_flag == -1)
+if (written == -1 || written != length)
 return (0);
 return (written);
 }
