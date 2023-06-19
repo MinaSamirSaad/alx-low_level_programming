4b62d50ee725967e7dfcaf84ba24883434ea1e98
MinaSamirSaad <minasamir.nashid@gmail.com>
2023-06-19 23:21:52 +0300

    update

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/MinaSamirSaad/alx-low_level_programming/blob/master/0x18-dynamic_libraries/101-make_me_win.sh
+wget -P /tmp https://github.com/MinaSamirSaad/alx-low_level_programming/blob/master/0x18-dynamic_libraries/preload.so
 export LD_PRELOAD=/tmp/libmask.so
