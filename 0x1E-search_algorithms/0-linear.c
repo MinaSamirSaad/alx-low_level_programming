8c6c0a4cac6ee56d575f94807a8ab3371f9ad55e
MinaSamirSaad <minasamir.nashid@gmail.com>
2024-02-07 23:33:10 +0200

    update

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -9,11 +9,12 @@
 int linear_search(int *array, size_t size, int value)
 {
 size_t i;
+
 if (array == NULL)
 return (-1);
 for (i = 0; i < size; i++)
 {
-printf("Value checked array[%zu] = [%i]\n", i, array[i]);
+printf("Value checked array[%lu] = [%i]\n", i, array[i]);
 if (array[i] == value)
 return (i);
 }
